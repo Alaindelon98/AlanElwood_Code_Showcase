ee382d19f9f67df1cccae0c2826214e725f6734e
Alaindelon98 <alanelya@gmail.com>
2019-02-06 14:41:52 +0100

    Showcase
    These are all the scripts and screenshots of the scene I created as an assigment for a UPF TFG. Sadly the scene itself dones't run on unity 2018 so I can't upload it here at the moment.

---
+++ SceneSchangeScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSchangeScript : MonoBehaviour {
+
+    public bool forceIt;
+
+    private void Update()
+    {
+        ForceVisible(forceIt);
+    }
+    public void StartAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void GoToScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void VisibleCursor(bool visible)
+    {
+        Cursor.visible = visible;
+        if(visible)
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+
+        }
+    }
+
+    public void ForceVisible(bool forceIt)
+    {
+        if (forceIt)
+        {
+            if (Input.GetMouseButtonDown(0) && !Cursor.visible)
+            {
+                VisibleCursor(true);
+            }
+        }
+    }
+}
